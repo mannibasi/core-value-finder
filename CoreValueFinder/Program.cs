f37f6a008af9ef4b885fc6a5241570cb75a90bed
= <manni.basi@gmail.com>
2020-09-27 16:29:36 +0100

    Initial project setup

---
+++ CoreValueFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreValueFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Core Value Finder! http://www.mypersonalimprovement.com/personalcorevalues.shtml");
+        }
+    }
+}
---
+++ CoreValueFinderTests/CoreValueFinderTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreValueFinderTests
+{
+    [TestClass]
+    public class CoreValueFinderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
